a6da319721b2cfda6969ea574faaf9eb38ce1279
Vigikaran <vigikaran.php@gmail.com>
2016-08-13 17:04:07 +0100

    422 Error Handled (#1)
    * Content Type Adjust for Support HTML 5 FormData
    
    * 422 Error Code Handled ,  JSON Request Header Added
    
    * Namespace misplaced Fixed
    
    * else if added

--- ajax.js
+++ ajax.js
@@ -2,7 +2,14 @@ var ajax = {
 	get: function(e, t) {
 		var s = new XMLHttpRequest;
 		s.addEventListener("readystatechange", function() {
-			4 == this.readyState && 200 == this.status ? t(!1, this.response, !1) : 404 == this.status && t({"code":"1","message":"page not found","status":"404"}, !1, !1)
+		if(4 == this.readyState && 200 == this.status){
+				t(!1, this.response, !1);
+			}
+			else if(422 == this.status){
+				t(!0, this.response, !1);
+			}else if(404 == this.status){
+				t({"code":"1","message":"page not found","status":"404"}, !1, !1);
+			}
 		}), s.addEventListener("progress", function(e) {
 			e.lengthComputable ? t(!1, !1, {
 				loaded: e.loaded,
@@ -19,13 +26,19 @@ var ajax = {
 	post: function(e, t, s) {
 		var n = new XMLHttpRequest;
 		n.addEventListener("readystatechange", function() {
-			4 == this.readyState && 200 == this.status ? s(!1, this.response, !1) : 404 == this.status && s({"code":"1","message":"page not found","status":"404"}, !1, !1)
+		if(4 == this.readyState && 200 == this.status){
+				s(!1, this.response, !1);
+			}else if(422 == this.status){
+				s(!0, this.response, !1);
+			}else if(404 == this.status){
+				s({"code":"1","message":"page not found","status":"404"}, !1, !1);
+			} 
 		}), n.addEventListener("progress", function(e) {
 			e.lengthComputable ? s(!1, !1, {
 				loaded: e.loaded,
 				fileSize: e.total
 			}) : s(!1, !1, !1)
-		}), n.open("POST", e, !0), n.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"), n.send(t)
+		}), n.open("POST", e, !0), n.setRequestHeader('Accept', 'application/json'), n.send(t)
 	},
 	postSync:function(ad,dt){
         var a=new XMLHttpRequest;
